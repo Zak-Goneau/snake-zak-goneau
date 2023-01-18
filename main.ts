be39535ceab9171399ae3ec3077e5343d90e2159
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-17 19:10:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -42,6 +42,9 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         speedY = 0
     }
 })
+sprites.onOverlap(SpriteKind.Food, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.setFlag(SpriteFlag.AutoDestroy, true)
+})
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     if (speedX == 0) {
         speedX = 1
@@ -56,11 +59,50 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     otherSprite.destroy()
+    music.powerUp.play()
     growth = 1
     spawnFood()
     info.changeScoreBy(1)
 })
 sprites.onDestroyed(SpriteKind.Player, function (sprite) {
+    game.over(false)
+    music.wawawawaa.play()
+})
+function spawnrock () {
+    mySprite = sprites.create(img`
+        . . . . . c c b b b . . . . . . 
+        . . . . c b d d d d b . . . . . 
+        . . . . c d d d d d d b b . . . 
+        . . . . c d d d d d d d d b . . 
+        . . . c b b d d d d d d d b . . 
+        . . . c b b d d d d d d d b . . 
+        . c c c c b b b b d d d b b b . 
+        . c d d b c b b b b b b b b d b 
+        c b b d d d b b b b b d d b d b 
+        c c b b d d d d d d d b b b d c 
+        c b c c c b b b b b b b d d c c 
+        c c b b c c c c b d d d b c c b 
+        . c c c c c c c c c c c b b b b 
+        . . c c c c c b b b b b b b c . 
+        . . . . . . c c b b b b c c . . 
+        . . . . . . . . c c c c . . . . 
+        `, SpriteKind.Enemy)
+    isEmptyPosition = false
+    while (!(isEmptyPosition)) {
+        x = 8 + 16 * randint(0, 9)
+        y = 8 + 15 * randint(0, 7)
+        isEmptyPosition = true
+        for (let value of snake) {
+            if (x == value.x && y == value.y) {
+                isEmptyPosition = false
+            }
+        }
+    }
+    mySprite.setPosition(x, y)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.ashes, 500)
+    mySprite.destroy(effects.ashes, 500)
     game.over(false)
     music.wawawawaa.play()
 })
@@ -75,41 +117,42 @@ let mySprite: Sprite = null
 music.playMelody("C5 C5 B A C5 B G B ", 240)
 game.splash("Welcome to Snake Game!" + " Press A to Start!")
 tiles.setCurrentTilemap(tilemap`level1`)
+info.setScore(0)
 mySprite = sprites.create(img`
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
     6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
     6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
     `, SpriteKind.Player)
 mySprite.setFlag(SpriteFlag.AutoDestroy, true)
 mySprite.setPosition(8, 8)
 snake.push(mySprite)
 mySprite = sprites.create(img`
     6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-    6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
     6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
     `, SpriteKind.Player)
 mySprite.setFlag(SpriteFlag.AutoDestroy, true)
@@ -119,32 +162,29 @@ speedX = 1
 speedY = 0
 growth = 0
 spawnFood()
-info.setScore(0)
+spawnrock()
 pause(2000)
-forever(function () {
-    music.playMelody("C5 A B G B A G C5 ", 240)
-})
 forever(function () {
     if (growth == 0) {
         mySprite = snake.shift()
     } else {
         growth = 0
         mySprite = sprites.create(img`
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
             6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
-            6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
             6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+            6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+            6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+            6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
             `, SpriteKind.Player)
         mySprite.setFlag(SpriteFlag.AutoDestroy, true)
     }
@@ -154,6 +194,7 @@ forever(function () {
     snake.push(mySprite)
     if (snake.length == 80) {
         game.over(true)
+        music.beamUp.play()
     }
     pause(200)
 })
@@ -163,4 +204,12 @@ forever(function () {
         game.over(false)
         music.wawawawaa.play()
     }
+})
+forever(function () {
+    pause(15000)
+    spawnrock()
+})
+forever(function () {
+    music.playMelody("C5 A B G B A G C5 ", 240)
+    music.playMelody("B G A B G F B A ", 240)
 })
